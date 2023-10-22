1c6673f02cf6dc082b5d60d933b04ea50f322cec
lucasalc25 <lucas.alc25@gmail.com>
2023-10-22 15:44:43 -0400

    19.2
    Ajuste na sobreposição do menu mobile e remoção de credenciais pra login para melhor realização de testes

--- script.js
+++ script.js
@@ -26,17 +26,7 @@ menuToggle.onclick = function() {
 }
 
 function login() {
-    const cpf = document.getElementById('cpf').value;
-    const senha = document.getElementById('senha').value;
-
-    // Simulando autenticação
-    if (cpf === '123' && senha === '1234') {
-        location.href = 'home_coord.html';
-    } else if (cpf === '321' && senha === '1234') {
-        location.href = 'home_prof.html';
-    } else {
-        alert('CPF ou senha incorretos. Tente novamente.');
-    }
+  location.href = 'home_coord.html';
 }
 
 
